1881fe14d0b3b9aae0683711c2d7dd3693c2e1ad
sametyldrmm <yildirimsamet051@gmail.com>
2022-11-23 00:38:35 +0300

    yeni başlangıçlar

--- deneme.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    std =[]
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        std.append((score,name))
-    std.sort()
-    print(std)
